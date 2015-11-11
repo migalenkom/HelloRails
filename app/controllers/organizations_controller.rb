3a2ecbe6030cf9ca17dc8985152c19a657f8560d
Mike Mygalenko <m.mygalenko@svitla.com>
2015-11-11 16:05:25 +0200

    ssl test

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,8 +1,12 @@
 class OrganizationsController < ApplicationController
 
   def index
+
     @organizations = Organization.my_organizations(current_user)
     @shared_organizations = Organization.shared_organizations(current_user)
+    logger.info "#{request.protocol}"
+    puts request.ssl?
+
     respond_to do |format|
       format.html # index.html.erb
       format.js {}
